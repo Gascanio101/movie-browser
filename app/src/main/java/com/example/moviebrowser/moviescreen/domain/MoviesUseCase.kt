53f1c75a6f1fff3775e8b1a572a27ac16113141a
Gabriel Ascanio <80049275+Gascanio101@users.noreply.github.com>
2023-03-07 12:58:59 +0100

    minor fixes

--- app/src/main/java/com/example/moviebrowser/moviescreen/domain/MoviesUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviebrowser.moviescreen.domain
-
-/*
-class MoviesUseCase {
-    private val repository = MoviesRepository()
-    suspend operator fun invoke(): PopularMoviesResponse? {
-        return repository.getPopularMovies()
-    }
-}*/
